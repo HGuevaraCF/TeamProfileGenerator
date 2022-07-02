96fa1929f37b87655225c5dc72a63b02b793b64b
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-07-02 16:15:01 -0500

    remove styles

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ function createHTML(cardsContent) {
             <link rel="stylesheet" href="./style.css">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+            <style>
+            .row>* {
+                padding-left: 0% !important;
+                padding-right: 0% !important;
+            }
+    
+            .row {
+                justify-content: center;
+            }
+    
+            .title {
+                background-color: #0081a7 !important;
+            }
+    
+            .info {
+                background-color: #00afb9 !important;
+            }
+        </style>
         </head>
         <body>
             <div class="py-2 mb-4 text-center title text-white">
